e37e68cf7b7f0045b17f3fb6b9875ee5d3d5b136
JP Lew <jp@hackcapital.com>
2019-11-25 12:01:15 -0800

    add globals.d.ts file to prevent Typescript compilation errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,31 @@
-import React, { Component } from 'react'
-import content from '../content/home.md';
+import React, { Component } from "react"
+import content from "../content/home.md"
+
+type Cat = {
+  name: string
+  description: string
+}
 
 export default class Home extends Component {
-
   render() {
-    console.log({content})
-    let { html , attributes:{ title, cats } } = content;
+    const {
+      html,
+      attributes: { title, cats }
+    } = content
+
     return (
       <article>
-          <h1>{title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: html }}/>
-          <ul>
-              { cats.map((cat, k) => (
-                  <li key={k}>
-                    <h2>{cat.name}</h2>
-                    <p>{cat.description}</p>
-                  </li>
-              ))}
-          </ul>
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <ul>
+          {cats.map((cat: Cat, k: number) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
       </article>
     )
   }
 }
-
