eb213607481497bde0147d6dafbb5a082bb6ca35
JP Lew <jp@hackcapital.com>
2019-11-25 22:28:00 -0800

    add links to test page

--- pages/posts/test.tsx
+++ pages/posts/test.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link"
+
 export default () => {
   return (
     <article>
       <h1>Test</h1>
+      <p>
+        <Link href="/posts/[slug]" as="/posts/lets-party">
+          Let's Party
+        </Link>
+      </p>
+      <p>
+        <Link href="/posts/[slug]" as="/posts/some-food-for-thought">
+          Some Food For Thought
+        </Link>
+      </p>
     </article>
   )
 }
